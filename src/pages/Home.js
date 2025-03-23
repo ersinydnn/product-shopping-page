7ff04a8486815d0d85c26fea6dcae7624821480e
Ersin Aydın <youremail@example.com>
2025-03-23 23:25:30 +0300

    updated responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,9 +35,10 @@ const Home = () => {
 
   return (
     <div className="max-w-6xl mx-auto p-4">
-      <div className="flex justify-center gap-4 mb-6">
+      {/* Kategori Seçme Menüsü - Responsive */}
+      <div className="flex overflow-x-auto scrollbar-hidden gap-4 mb-6 px-2 sm:px-4">
         <button
-          className={`px-6 py-3 border rounded-md font-semibold transition 
+          className={`px-6 py-3 whitespace-nowrap border rounded-md font-semibold transition 
                       transform hover:scale-110 duration-200 ease-in-out
                       ${
                         selectedCategory === "all"
@@ -51,7 +52,7 @@ const Home = () => {
         {categories.map((category) => (
           <button
             key={category}
-            className={`px-4 py-2 border rounded-md font-semibold transition 
+            className={`px-4 py-2 whitespace-nowrap border rounded-md font-semibold transition 
                         transform hover:scale-110 duration-200 ease-in-out 
                         ${
                           selectedCategory === category
